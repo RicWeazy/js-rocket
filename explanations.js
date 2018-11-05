dc29307fd12c5ddfcdda392ae017624242146765
RicWeazy <44574324+RicWeazy@users.noreply.github.com>
2018-11-05 17:35:33 +0200

    Some explanations of Variables

---
+++ explanations.js
@@ -0,0 +1,13 @@
+// Explanations of the Variables
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = false;
+var richObject = {
+	firstName: 'Rich',
+	lastname: 'Armstrong',
+	favcolors: ['blue', 'red', 'green'],|
+	yearsAlive: 103,
+	isMale: true
+};
+
